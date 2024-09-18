0276eb0b3d96c82d4f1f289ca951f82cb378629c
metalstyle545@gmail.com <131258361+AlexLis312@users.noreply.github.com>
2024-09-18 16:31:23 +0300

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,5 +2,5 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", () => {
-  output.textContent = input.value;
+  !input.value.length ? (output.textContent = "Anonymous") : (output.textContent = input.value);
 });
